e214a3ee97d420d6345cbb6f414260b0030e4cdc
extreme-coder <aryan.singh.lamba@gmail.com>
2024-09-24 20:45:23 -0700

    get days left and custom data

--- src/api/entry/controllers/entry.js
+++ src/api/entry/controllers/entry.js
@@ -6,4 +6,32 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::entry.entry');
+module.exports = createCoreController('api::entry.entry', ({ strapi }) => ({
+    async find(ctx) {
+        const { data, meta } = await super.find(ctx)
+        const newData = Object.values(data).map(async d => ({
+            ...d,
+            custom: null,
+            ...d.custom,
+            daysLeft: d.due && await strapi.service('api::entry.entry').daysLeft(d.due)
+        })).reduce((acc, curr, index) => {
+            acc[index] = curr;
+            return acc;
+        }, {})
+        console.log(newData)
+        return { data: newData, meta }
+    },
+
+    async findOne(ctx) {
+        const { data, meta } = await super.findOne(ctx)
+        return {
+            data: {
+                ...data,
+                custom: null,
+                ...data.custom,
+                daysLeft: d.due && await strapi.service('api::entry.entry').daysLeft(data.due)
+            },
+            meta
+        }
+    },
+}));
--- src/api/entry/services/entry.js
+++ src/api/entry/services/entry.js
@@ -6,4 +6,11 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::entry.entry');
+module.exports = createCoreService('api::entry.entry', ({ strapi }) => ({
+    async daysLeft(deadline) {
+        const currentDate = new Date();
+        const deadlineDate = new Date(deadline);
+        const dayDiff = 1 + Math.ceil((deadlineDate - currentDate) / (1000 * 60 * 60 * 24));
+        return dayDiff
+    }
+}))
